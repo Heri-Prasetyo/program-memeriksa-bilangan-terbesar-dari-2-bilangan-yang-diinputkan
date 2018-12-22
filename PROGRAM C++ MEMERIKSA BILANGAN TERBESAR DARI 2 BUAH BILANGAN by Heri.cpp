c3308a27c2bc7f97b12d561eefab51b4102f5ff5
Heri Prasetyo <45659342+Heri-Prasetyo@users.noreply.github.com>
2018-12-22 10:39:12 +0700

    Add files via upload

---
+++ PROGRAM C++ MEMERIKSA BILANGAN TERBESAR DARI 2 BUAH BILANGAN by Heri.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a,b;
+
+cout<<"PROGRAM MEMERIKSA BILANGAN TERBESAR DARI 2 BUAH BILANGAN"<<endl;
+
+cout<<"Masukan Nilai A = ";
+cin>>a;
+cout<<"Masukan Nilai B = ";
+cin>>b;
+if(a<b){
+    cout<<"B terbesar\n";}
+else
+    if(a>b){
+    cout<<"A terbesar\n";}
+else
+
+    cout<<"eror";
+    return 0;
+
+
+
+
+
+}
+
+
+
+
+
